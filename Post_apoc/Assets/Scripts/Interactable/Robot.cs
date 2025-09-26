63db765a7f5be89bf9255e7c9d8baf9a9fd33161
SyrusXRW <Ittybitty100s@gmail.com>
2025-09-26 00:30:03 -0700

    Added in Yarnspinner- Made Interaction, Custom View

--- Post_apoc/Assets/Scripts/Interactable/Robot.cs
+++ Post_apoc/Assets/Scripts/Interactable/Robot.cs
@@ -1,27 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Robot : MonoBehaviour, IInteractable
+
+namespace Yarn.Unity.Example
 {
-     [SerializeField] private string _prompt;
-
-     public Item item;
-
-     public PlayerController playercontroller;
-
-     public string InteractionPrompt => _prompt;
-
-     public bool Interact(Interactor interactor)
+     public class Robot : MonoBehaviour, IInteractable
      {
-          PickUp();
-          Debug.Log(message: "opening chest!");
-          return true;
-     }
-     void PickUp()
-     {
-          playercontroller.Add(item);
-          Debug.Log("Picking up" + item.name);
-          Destroy(gameObject);
+          [SerializeField] private string _prompt;
+
+          public Item item;
+
+          public PlayerController playercontroller;
+
+          public string InteractionPrompt => _prompt;
+
+          public bool Interact(Interactor interactor)
+          {
+               PickUp();
+               Debug.Log(message: "opening chest!");
+               return true;
+          }
+          void PickUp()
+          {
+               playercontroller.Add(item);
+               Debug.Log("Picking up" + item.name);
+               Destroy(gameObject);
+          }
      }
 }
-
