9b90ec5cc740f71c8390870f16c11306a8e2c924
SyrusXRW <Ittybitty100s@gmail.com>
2025-09-26 04:16:04 -0700

    Erm

--- Post_apoc/Assets/Scripts/Character_System/PlayerController.cs
+++ Post_apoc/Assets/Scripts/Character_System/PlayerController.cs
@@ -10,9 +10,25 @@ namespace Yarn.Unity.Example
         public Rigidbody theRB;
         public float moveSpeed, jumpForce;
         
+<<<<<<< Updated upstream
         public float interactionRadius = 2.0f;
         private Vector2 moveInput;
 
+=======
+<<<<<<< Updated upstream
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if (DialogueManager.Instance.isDialogueActive)
+        //{
+=======
+        public float interactionRadius = 2.0f;
+        private Vector2 moveInput;
+
+        public Animator anim; 
+
+>>>>>>> Stashed changes
         public LayerMask whatIsGround;
         public Transform groundPoint;
         private bool isGrounded;
@@ -41,6 +57,10 @@ namespace Yarn.Unity.Example
         {
             //if (DialogueManager.Instance.isDialogueActive)
             //{
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
             //moveSpeed = 5f;
             //Debug.Log(message: "Pls");
             //return;
@@ -65,11 +85,23 @@ namespace Yarn.Unity.Example
             moveInput.x = Input.GetAxisRaw("Horizontal");
             moveInput.y = Input.GetAxisRaw("Vertical");
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+        RaycastHit hit;
+        if(Physics.Raycast(groundPoint.position, Vector3.down, out hit, .3f, whatIsGround))
+=======
+>>>>>>> Stashed changes
             moveInput.Normalize();
 
             theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
             theRB.rotation = Quaternion.Euler(new Vector3(moveInput.x, theRB.velocity.y, theRB.velocity.z));
 
+<<<<<<< Updated upstream
+=======
+            anim.SetFloat("moveSpeed", theRB.velocity.magnitude);
+
+>>>>>>> Stashed changes
             RaycastHit hit;
             if (Physics.Raycast(groundPoint.position, Vector3.down, out hit, .3f, whatIsGround))
             {
@@ -106,6 +138,10 @@ namespace Yarn.Unity.Example
 
         }
         public void CheckForNearbyNPC()
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         {
             var allParticipants = new List<NPC>(FindObjectsOfType<NPC>());
             var target = allParticipants.Find(delegate (NPC p)
